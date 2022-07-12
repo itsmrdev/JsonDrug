a6d9e010a52a9b7e96666fbe9b94e6a8f16a7877
Mohit Suthar <mohit261.mindiii@gmail.com>
2022-07-12 10:23:12 +0530

    fix issue

--- astfun.php
+++ astfun.php
@@ -9,12 +9,12 @@ class CreateForm
 
             $isRequired = (isset($date->isRequired) && $date->isRequired) ? 'required' : '';
             $isReadonly = (isset($date->isReadonly) && $date->isReadonly) ? 'readonly' : '';
-            $unit = (isset($date->unit) && $date->unit) ? '('.$date->unit.')' : '';
+            $unit = (isset($date->unit) && $date->unit) ? '(' . $date->unit . ')' : '';
             $pattern = $date->type == 'date' ? 'pattern="\d{4}-\d{1,2}-\d{1,2}"' : '';
 
             $divStart = "<div class='form-input-div col-50'>";
 
-            $label = "<label class='label'>" . $date->label .$unit "</label>";
+            $label = "<label class='label'>" . $date->label . $unit . "</label>";
 
             $input = "<input name='" . $date->key . "' class='input-form' type='" . $date->type . "' $isRequired $isReadonly $pattern></input>";
 
@@ -30,11 +30,11 @@ class CreateForm
 
             $isRequired = (isset($number->isRequired) && $number->isRequired) ? 'required' : '';
             $isReadonly = (isset($number->isReadonly) && $number->isReadonly) ? 'readonly' : '';
-            $unit = (isset($number->unit) && $number->unit) ? '('.$number->unit.')' : '';
+            $unit = (isset($number->unit) && $number->unit) ? '(' . $number->unit . ')' : '';
 
             $divStart = "<div class='form-input-div col-50'>";
 
-            $label = "<label class='label'>" . $number->label .$unit "</label>";
+            $label = "<label class='label'>" . $number->label . $unit . "</label>";
 
             $input = "<input name='" . $number->key . "' class='input-form' type='" . $number->type . "' $isRequired $isReadonly></input>";
 
@@ -49,10 +49,10 @@ class CreateForm
         if (isset($dropdown->type) && $dropdown->type == 'dropdown') {
             $isRequired = (isset($dropdown->isRequired) && $dropdown->isRequired) ? 'required' : '';
             $isReadonly = (isset($dropdown->isReadonly) && $dropdown->isReadonly) ? 'readonly' : '';
-            $unit = (isset($dropdown->unit) && $dropdown->unit) ? '('.$dropdown->unit.')' : '';
+            $unit = (isset($dropdown->unit) && $dropdown->unit) ? '(' . $dropdown->unit . ')' : '';
             $divStart = "<div class='form-input-div col-50'>";
 
-            $label = "<label class='label'>" . $dropdown->label .$unit "</label>";
+            $label = "<label class='label'>" . $dropdown->label . $unit . "</label>";
 
             $selectOpen = "<select name='" . $dropdown->key . "' class='input-form dropdown' $isRequired $isReadonly>";
             $options = '';
